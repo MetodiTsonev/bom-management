3542828ebab4a8add2a8a280114667ec28355493
Vladimir <vladimir.grig02@gmail.com>
2024-11-22 18:34:55 +0200

    Progress on Products page

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Description from '../Description';
 import Table from '../Table';
@@ -6,31 +6,81 @@ import Search from '../Search';
 import Button from '../Button';
 
 import './PageStyle.css';
-
+import ProductForm from "./ProductForm";
 
 function Products() {
-  const data = [
-    { id: 1, name: 'Product 1', amount: 100 },
-    { id: 2, name: 'Product 2', amount: 200 },
-    { id: 3, name: 'Product 3', amount: 300 },
-  ];
+  const [filteredData, setFilteredData] = useState([]);
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [isAddVisible, setIsAddVisible] = useState(false);
+  const headers = {ID: `PRODUCT_ID`, Name: `PRODUCT_NAME`, Description: `PRODUCT_DESCRIPTION`};
+
+  const fetchData = () => {
+    fetch('http://localhost:5001/api/products')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log("Fetched data:", data); // Log to verify data
+          setData(data);
+          setFilteredData(data);
+        })
+        .catch(error => console.error("Error fetching products:", error));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSearch = () => {
+    setFilteredData(data.filter((row) =>
+      Object.values(row).some((value) =>
+        value.toString().toLowerCase().includes(searchValue.toLowerCase())
+      )
+    ));
+  }
+
+  const handleClear = () => {
+    setFilteredData(data);
+  }
+
+  const handleAdd = () => {
+    setIsAddVisible(true);
+  }
+
+  const handleClose = () => {
+    setIsAddVisible(false);
+    console.log("Close clicked");
+  }
+
+  const handleSubmit = (product) => {
+    fetchData();
+    handleClose();
+  }
+
   return (
     <div>
       <Description text="Products" description="The products page is used for configuring product details" />
       <div className="container">
         <div className="left-column">
-          <Table data={data} />
+          <Table data={filteredData} onRowSelect={setSelectedRow} headers={headers}/>
+          <Button label="Clear filters" onClick={handleClear} type="clear"/>
         </div>
         <div className="right-column">
           <div className="searchField">
-            <Search />
-            <Button label="Go" onClick={() => alert('Search clicked')} type="search" />
+            <Search onSearch={setSearchValue}/>
+            <Button label="Go" onClick={handleSearch} type="search"/>
           </div>
-          <Button label="Add" onClick={() => alert('Add clicked')} type="add" />
-          <Button label="Edit BOM Specs" onClick={() => alert('Edit clicked')} type="delete" />
-          <Button label="View Production Cost" onClick={() => alert('View clicked')} type="edit" />
+          <Button label="Add" onClick={handleAdd} type="add" />
+          <Button label="Edit BOM Specs" onClick={() => alert('Edit clicked')} type="editBom" />
+          <Button label="View Production Cost" onClick={() => alert('View clicked')} type="delete" />
         </div>
       </div>
+      {isAddVisible && <ProductForm onCLose={handleClose} onSubmit={handleSubmit} editObject={null}/>}
     </div>
   );
 }
